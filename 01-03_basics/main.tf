b434b58d73579d20b00f01fda1b26d45521041e7
Xiaoli Shen <alabebop@gmail.com>
2023-08-15 17:46:17 +0900

    format

--- 01-03_basics/main.tf
+++ 01-03_basics/main.tf
@@ -1,23 +1,24 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.16"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
+  }
 
-    required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 }
 
+
 provider "aws" {
-    region = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
 resource "aws_instance" "app_server" {
-    ami = "ami-04beabd6a4fb6ab6f"
-    instance_type = "t2.micro"
+  ami           = "ami-04beabd6a4fb6ab6f"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "ExampleAppServerInstance"
-    }
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
